ad4fdfb0e620fc26ad3155ee7270dd51dd2a42b2
LayloMirdadoeva <120080211+LayloMirdadoeva@users.noreply.github.com>
2024-04-15 16:34:47 +0500

    Added Controllers for WebApi

---
+++ Application/Common/Mapping/OrderMapProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mapping;
+public class OrderMapProfile : Profile
+{
+    public OrderMapProfile()
+    {
+        CreateMap<CreateOrderRequest, Order>();
+        CreateMap<Order, SingleOrderResponse>();
+
+    }
+}
---
+++ WebApi/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using Contracts.Requests;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderService _orderServise;
+        public OrderController(OrderService orderServise)
+        {
+            _orderServise = orderServise;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> Create([FromBody] CreatOrderRequest request, CancellationToken cancellationToken)
+        {
+            var createdOrder = await _orderService.CreateAsync(request, cancellationToken);
+            return Ok(createdOrder);
+        }
+    }
+}
